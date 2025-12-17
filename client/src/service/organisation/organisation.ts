15179bf19c0484a7589e693bd328cb3356418475
Infifrontend <infifrontend@gmail.com>
2025-12-17 05:05:06 +0000

    Organisation api integration changes

--- client/src/service/organisation/organisation.ts
+++ client/src/service/organisation/organisation.ts
@@ -4,8 +4,8 @@ const organisationService = CommonService.injectEndpoints({
   endpoints: (build) => ({
     getOrganisationData: build.query<any, any>({
       query: (params) => {
-        const { page = 1, page_size = 6 } = params || {};
-        return `api/management/organizations/?page=${page}&page_size=${page_size}`;
+        const { page = 1 } = params || {};
+        return `api/management/organizations/?page=${page}`;
       },
     }),
   }),
